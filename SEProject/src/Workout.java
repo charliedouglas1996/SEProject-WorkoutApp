333ac66dcab2b6088a55f29dde6ca9665d1c23cb
Elif Cudi <32675603+chronosXIV@users.noreply.github.com>
2018-03-08 13:24:16 +0000

    Editing Workout Class -- Test Push

--- SEProject/src/Workout.java
+++ SEProject/src/Workout.java
@@ -5,25 +5,16 @@ public class Workout {
 	private String workoutDescription;
 	private int duration;
 
-	/**
-	 * 
-	 * @param name
-	 * @param type
-	 * @param descrip
-	 * @param time
-	 */
-	public Workout(String name, boolean type, String descrip, int time) {
-		// TODO - implement Workout.Workout
-		throw new UnsupportedOperationException();
+	public Workout(String name, boolean workoutType, String descrip, int time) {
+		
 	}
 
 	public String getWorkoutName() {
 		return this.workoutName;
 	}
 
-	public String getWorkoutType() {
-		// TODO - implement Workout.getWorkoutType
-		throw new UnsupportedOperationException();
+	public boolean getWorkoutType() {
+		return this.workoutType;
 	}
 
 	public String getWorkoutDescription() {
@@ -34,48 +25,27 @@ public class Workout {
 		return this.duration;
 	}
 
-	/**
-	 * 
-	 * @param name
-	 */
 	public void setWorkoutName(String name) {
 		this.workoutName = name;
 	}
 
-	public void setWorkoutType() {
-		// TODO - implement Workout.setWorkoutType
-		throw new UnsupportedOperationException();
+	public void setWorkoutType(boolean type) {
+		this.workoutType = type;
 	}
 
-	/**
-	 * 
-	 * @param descrip
-	 */
 	public void setWorkoutDescription(String descrip) {
 		this.workoutDescription = descrip;
 	}
 
-	/**
-	 * 
-	 * @param time
-	 */
 	public void setDuration(int time) {
 		this.duration = time;
 	}
 
-	/**
-	 * 
-	 * @param ex
-	 */
 	public void addWExercise(Exercise ex) {
 		// TODO - implement Workout.addWExercise
 		throw new UnsupportedOperationException();
 	}
 
-	/**
-	 * 
-	 * @param ex
-	 */
 	public void removeWExercise(Exercise ex) {
 		// TODO - implement Workout.removeWExercise
 		throw new UnsupportedOperationException();
