c6efec1a068c6fed13292fa6cf3c25b8c0571225
Rhiannaa Singh <rhiannaasingh22@gmail.com>
2018-03-08 13:47:02 +0000

    testing

--- SEProject/src/SEProject.java
+++ SEProject/src/SEProject.java
@@ -15,7 +15,7 @@ public class SEProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        system.out.println("hello");
     }
     
 }
