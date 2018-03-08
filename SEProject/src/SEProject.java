5e1ff8b0699b6abc3499785a7eab5686007afb10
Elif Cudi <32675603+chronosXIV@users.noreply.github.com>
2018-03-08 13:47:37 +0000

    test on same java file

--- SEProject/src/SEProject.java
+++ SEProject/src/SEProject.java
@@ -16,6 +16,8 @@ public class SEProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("whatup..ok");
     }
     
 }
