da18f867a259ad6eec6005f0a0dac1b8425c15c4
Elif Cudi <32675603+chronosXIV@users.noreply.github.com>
2018-03-12 12:06:34 +0000

    format

--- SEProject/src/User.java
+++ SEProject/src/User.java
@@ -1,95 +1,100 @@
+
 import java.util.*;
 
 public class User {
 
-	private static User instance=new User();
-	private String userName;
-	private ArrayList<Float> userWeight;
-        private ArrayList<Float> userHeight;
-	private ArrayList<Workout> workoutList;
-	private ArrayList<Exercise> exerciseList;
+    private static User instance = new User();
+    private String userName;
+    private ArrayList<Float> userWeight;
+    private ArrayList<Float> userHeight;
+    private ArrayList<Workout> workoutList;
+    private ArrayList<Exercise> exerciseList;
 
-	public static User getInstance() {
-		return instance;
-	}
-
-    public void setName(String name) {
-        userName=name;
+    public static User getInstance() {
+        return instance;
     }
 
-	public String getName() {
-                return userName;
-	}
+    public void setName(String name) {
+        userName = name;
+    }
 
-	public ArrayList<Float> getWeight() {
-		return userWeight;
-	}
+    public String getName() {
+        return userName;
+    }
+
+    public ArrayList<Float> getWeight() {
+        return userWeight;
+    }
 
     public void addWeight(float weight) {
         userWeight.add((Float) weight);
     }
 
-	public ArrayList<Float> getHeight() {
-		return userHeight;
-	}
+    public ArrayList<Float> getHeight() {
+        return userHeight;
+    }
 
-	public void addHeight(float height) {
+    public void addHeight(float height) {
         userHeight.add((Float) height);
-	}
+    }
 
     // display a list of all exercises
-	public void displayExercises(){
-	    for(int i=0;i<exerciseList.size();i++){
-	        System.out.println("Exercise "+(i+1)+": "+exerciseList.get(i).getExerciseName());
+    public void displayExercises() {
+        for (int i = 0; i < exerciseList.size(); i++) {
+            System.out.println("Exercise " + (i + 1) + ": " + exerciseList.get(i).getExerciseName());
         }
     }
 
-	// show information about a specific exercise
-	public void viewExerciseInfo(Exercise ex) {
-        System.out.println(ex.getExerciseName()+", "+ex.getDescription()+", "+ex.getImage()+", "+ex.getLink()+", "+ex.getNotes()+", "+ex.getEquipment());
+    // show information about a specific exercise
+    public void viewExerciseInfo(Exercise ex) {
+        System.out.println(ex.getExerciseName() + ", " + ex.getDescription() + ", " + ex.getImage() + ", " + ex.getLink() + ", " + ex.getNotes() + ", " + ex.getEquipment());
     }
 
-	// adds a created exercise to the list of exercises
-	public void addExercise(Exercise ex) {
-		exerciseList.add(ex);
-	}
+    // adds a created exercise to the list of exercises
+    public void addExercise(Exercise ex) {
+        exerciseList.add(ex);
+    }
 
-	// TODO - edit the details of a specific exercise
-	public void editExercise(Exercise ex) {}
+    // TODO - edit the details of a specific exercise
+    public void editExercise(Exercise ex) {
+    }
 
-	// removes an exercise from the list of exercises
-	public void removeExercise(Exercise ex) {
-		exerciseList.remove(ex);
-	}
-    
-    public Exercise selectExercise(){
+    // removes an exercise from the list of exercises
+    public void removeExercise(Exercise ex) {
+        exerciseList.remove(ex);
+    }
+
+    public Exercise selectExercise() {
         return null;
     }
 
-	public void displayWorkouts() {
-        for(int i=0;i<workoutList.size();i++){
-            System.out.println("Workout "+(i+1)+": "+workoutList.get(i).getWorkoutName());
+    public void displayWorkouts() {
+        for (int i = 0; i < workoutList.size(); i++) {
+            System.out.println("Workout " + (i + 1) + ": " + workoutList.get(i).getWorkoutName());
         }
-	}
+    }
 
-	public void viewWorkoutInfo(Workout w) {
-		System.out.println(w.getWorkoutName()+", "+w.getWorkoutDescription()+", "+w.getWorkoutDuration()+", "+w.getWorkoutType());
-	}
+    public void viewWorkoutInfo(Workout w) {
+        System.out.println(w.getWorkoutName() + ", " + w.getWorkoutDescription() + ", " + w.getWorkoutDuration() + ", " + w.getWorkoutType());
+    }
 
-	public void addWorkout(Workout w) {
-		workoutList.add(w);
-	}
+    public void addWorkout(Workout w) {
+        workoutList.add(w);
+    }
 
-	// TODO - edit a specific workout
-	public void editWorkout(Workout w) {}
+    // TODO - edit a specific workout
+    public void editWorkout(Workout w) {
+    }
 
-	public void removeWorkout(Workout w) {
-		workoutList.remove(w);
-	}
+    public void removeWorkout(Workout w) {
+        workoutList.remove(w);
+    }
 
-	public Workout selectWorkout(int n) {
-        if(n<workoutList.size()) return workoutList.get(n);
+    public Workout selectWorkout(int n) {
+        if (n < workoutList.size()) {
+            return workoutList.get(n);
+        }
         System.out.println("Wrong workout selected");
         return null;
-	}
+    }
 }
