2a22a948602db54d671f13463310f47a973ebe19
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2018-03-12 12:07:22 +0000

    Exercise + ExerciseStats formating

--- SEProject/src/Exercise.java
+++ SEProject/src/Exercise.java
@@ -15,7 +15,8 @@ public abstract class Exercise {
 		setLink(hyperlink);
 		setNotes(newnote);
 		setEquipment(equip);
-		Exercisestats.setCount(c);
+		ExerciseStats stats = new ExerciseStats();
+		stats.setCount(c);
 		}
 
 	public String getExerciseName() {
@@ -42,50 +43,30 @@ public abstract class Exercise {
 		return this.equipment;
 	}
 
-	/**
-	 * 
-	 * @param name
-	 */
+
 	public void setExerciseName(String name) {
 		this.exerciseName = name;
 	}
 
-	/**
-	 * 
-	 * @param descrip
-	 */
+
 	public void setDescription(String descrip) {
 		this.description = descrip;
 	}
 
-	/**
-	 * 
-	 * @param imglink
-	 */
+
 	public void setImage(String imglink) {
 		this.imageLink = imglink;
 	}
 
-	/**
-	 * 
-	 * @param hyperlink
-	 */
 	public void setLink(String hyperlink) {
 		this.link = hyperlink;
 	}
 
-	/**
-	 * 
-	 * @param newnote
-	 */
 	public void setNotes(String newnote) {
 		this.notes = newnote;
 	}
 
-	/**
-	 * 
-	 * @param equip
-	 */
+
 	public void setEquipment(String equip) {
 		this.equipment = equip;
 	}
